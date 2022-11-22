8743b9a30b0d9bd28d1874e40a1bf104366130b7
Mohammed Shoaib <shoaib98libra@gmail.com>
2022-11-21 22:32:33 -0800

    Cut the number of blocks by half

--- src/utils.js
+++ src/utils.js
@@ -1,3 +1,3 @@
-export const NUM_BLOCKS = 5;
-export const NUM_TRIALS = 2;
-export const CONTROL_NUM_TRIALS = 2;
+export const NUM_BLOCKS = 10;
+export const NUM_TRIALS = 20;
+export const CONTROL_NUM_TRIALS = 42;
