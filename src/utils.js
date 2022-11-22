d874df58b9aa28440292e83efbc0075d25dc4541
Mohammed Shoaib <shoaib98libra@gmail.com>
2022-11-21 19:10:13 -0800

    Add constants for number of blocks, trials, and control trials

--- src/utils.js
+++ src/utils.js
@@ -2,3 +2,7 @@ export const CORRECT_KEY_PRESS = "ArrowLeft";
 export const CORRECT_KEY_TEXT = "left arrow key";
 export const WRONG_KEY_PRESS = "ArrowRight";
 export const WRONG_KEY_TEXT = "right arrow key";
+
+export const NUM_BLOCKS = 10;
+export const NUM_TRIALS = 20;
+export const CONTROL_NUM_TRIALS = 42;
