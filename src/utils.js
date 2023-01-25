f5bfee482a76b4b9cdef0048f90e9bc3c1a3b37e
Mia Fuentes <miafuen@stanford.edu>
2023-01-25 10:48:20 -0800

    add constant for video width and height

--- src/utils.js
+++ src/utils.js
@@ -3,3 +3,5 @@ export const NUM_TRIALS = 20;
 export const CONTROL_NUM_TRIALS = 42;
 export const SHOW_CONTROL_TRIALS = false;
 export const STIMULUS_FONT_SIZE = 80;
+export const VIDEO_WIDTH = 1238;
+export const VIDEO_HEIGHT = 800;
