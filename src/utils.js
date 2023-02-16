147843e1870fc5d3289ad1f8b2521ed9a683ec14
Mia Fuentes <miafuen@stanford.edu>
2023-02-16 10:23:36 -0800

    add constants for adaptive & practice num trials, video w/h

--- src/utils.js
+++ src/utils.js
@@ -1,7 +1,8 @@
 export const NUM_BLOCKS = 10;
-export const NUM_TRIALS = 20;
+export const ADAPTIVE_NUM_TRIALS = 20;
+export const PRACTICE_NUM_TRIALS = 10;
 export const CONTROL_NUM_TRIALS = 42;
 export const SHOW_CONTROL_TRIALS = false;
 export const STIMULUS_FONT_SIZE = 80;
-export const VIDEO_WIDTH = 1238;
-export const VIDEO_HEIGHT = 800;
+export const VIDEO_WIDTH = 1440;
+export const VIDEO_HEIGHT = 1800;
