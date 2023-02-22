3e93b849dd589a683373719e31fa12946fa40629
Mia Fuentes <miafuen@stanford.edu>
2023-02-22 15:01:04 -0800

    modified number of trials

--- src/utils.js
+++ src/utils.js
@@ -1,6 +1,6 @@
 export const NUM_BLOCKS = 10;
 export const ADAPTIVE_NUM_TRIALS = 20;
-export const PRACTICE_NUM_TRIALS = 10;
+export const PRACTICE_NUM_TRIALS = 8;
 export const CONTROL_NUM_TRIALS = 42;
 export const SHOW_CONTROL_TRIALS = false;
 export const STIMULUS_FONT_SIZE = 80;
